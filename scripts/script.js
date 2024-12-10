4eacaaf606543408dfaf0903c6c5394319195b0a
Stephany Oliveira da Silva <stephany16ce@outlook.com>
2024-12-10 11:37:54 -0300

    reparo no javascript e alteraçao de estilo

--- scripts/script.js
+++ scripts/script.js
@@ -5,25 +5,47 @@ const formInfo = document.getElementById('form');
 formInfo.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    //colentando valores dos inputs
+    //colentando valores dos inputs e spans
     const nome = document.getElementById('nome').value;
-    const altura = document.getElementById('altura').value;
-    const peso = document.getElementById('peso').value; 
-
-    //calculo
-    const imc = (peso / (altura * altura)).toFixed(2);
-
+    const altura = parseFloat(document.getElementById('altura').value);
+    const peso = parseFloat(document.getElementById('peso').value); 
     const resultado = document.getElementById('result');
-    let descricao = '';
+    const mensagem = document.getElementById('mensagem');
 
-    //impressao de resultados e condiçoes
+    //calculo do imc
+    const imc = (peso / (altura * altura)).toFixed(2);
+    let descricao = '';
+    let classificacao = '';
+
+    //verificando o preenchimento dos campos 
     if(nome != '' && altura != '' && peso != ''){
-        descricao = `Olá ${nome}!, acabei de calcular o seu imc e deu: ${imc}`
+        descricao = `Olá ${nome}!, o resultado do seu IMC é: ${imc}`
     }
     else{
         descricao = "Preencha todos os campos!"
     }
 
-    resultado.textContent = imc.replace('.',',');
-    document.getElementById('result').textContent = descricao;
+    //classificaçao de resultados
+    if(imc < 18.5){
+        classificacao = "Seu resultado apresenta que você está abaixo do peso."
+    }
+    else if ( imc >= 18.5 && imc < 24.9){
+        classificacao = "Seu resultado apresenta que seu peso é normal."
+    }
+    else if( imc >= 25 && imc < 29.9){
+        classificacao = "Seu resultado apresenta que você está com sobrepeso."
+    }
+    else if( imc >= 30 && imc < 34.9){
+        classificacao = "Seu resultado apresenta que você está com obesidade grau I."
+    }
+    else if(imc >= 35 && imc < 39.9){
+        classificacao = "Seu resultado apresenta que você está com obesidade grau II."
+    }
+    else{
+        descricao = "Preencha todos os campos!"
+    }
+
+    //exibindo a mensagem na tela
+    resultado.textContent = descricao.replace('.', ',');
+    mensagem.textContent = classificacao;
 })
